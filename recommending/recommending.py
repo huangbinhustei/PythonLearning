5ad70f06570358711f516284b0045905124f35fc
huangbinhustei <huangbinhustei@gmail.com>
2015-12-02 19:42:53 +0800

    del

--- recommending/recommending.py
+++ recommending/recommending.py
@@ -2,42 +2,41 @@
 # -*- coding: utf-8 -*-
 
 
-import xlrd
 import jieba
 import heapq
 import math
 import os
 
-path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))  + "/DATA/doclistmini.xlsx"
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))  + "/DATA/doclist_all.txt"
 
 # jieba.load_userdict(path)
 
-target = list(jieba.cut("天天炫斗布雷泽仓木熏布鲁三大职业对比分析攻略",cut_all = False))
-# target = ["天天炫斗","布雷泽","仓木熏","布鲁","三","大","职业","对比","分析"]
+target = list(jieba.cut("天天炫斗商城系统详解介绍攻略",cut_all = False))
 L2 = []
 
-with xlrd.open_workbook(path) as query:
-	table = query.sheets()[0] 
+
+with open(path,"r") as query:
+	table = query.readlines()
 	contain = []
 	shows = [x-x for x in range(len(target))]
 
-	for ii in range(table.nrows):
-		L1 =table.cell(ii,0).value
+	for ii in range(len(table)):
+		L1 = table[ii]
 		for jj in range(len(target)):
 			shows[jj] += L1.count(target[jj])
+			
 
 	for k in range(len(target)):
-		shows[k] = math.log(table.nrows/shows[k])
+		shows[k] = math.log(len(table)/shows[k])
+		if k < 3:
+			shows[k] += 3
 
-	for i in range(table.nrows):
-		L1 =table.cell(i,0).value
+	for i in range(len(table)):
+		L1 = table[i]
 		L2.append(L1)
 		contain.append(0)
 		for j in range(len(target)):
-#			if L1.count(target[j]) >0 :
-#				contain[i] += shows[j]
-
-			contain[i] += L1.count(target[j]) * shows[j]
+			contain[i] += L1.count(target[j]) * shows[j]/len(target)
 
 print(target)
 print(shows)
@@ -50,4 +49,6 @@ def sec(t):
 
 for_print = heapq.nlargest(10, hahahah, key= sec)
 
-print(for_print[:10])
+print("天天炫斗布雷泽仓木熏布鲁三大职业对比分析攻略")
+for xxx in for_print[:10]:
+	print(xxx)
