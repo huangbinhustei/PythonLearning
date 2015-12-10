fd9928d7578c4a4283a37109ed8516610b817eab
彬歆 <huangbinhustei@gmail.com>
2015-12-10 22:27:48 +0800

    可能游泳

--- gamenamemathc/grouping.py
+++ gamenamemathc/grouping.py
@@ -1,27 +1,25 @@
-game_name=[]
-long_game_name = []
+import os
+
+long_game_name = ["占坑"]
 short_game_name = []
+namelist = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)) + "/DATA/userdict/gamename.txt"
+flag = True
 
-with open("游戏名称.txt", "r", encoding="utf-8") as txt111:
-	for item in txt111.readlines():
-		game_name.append(item.strip())
+with open(namelist, "r", encoding="utf-8") as txt111:
+    for item in txt111:
+        new_game = item.strip()
+        for index, longer_name in enumerate(long_game_name):
+            if longer_name.find(new_game) > -1:
+                short_game_name.append(new_game)
+                flag = False
+                break
+            if new_game.find(longer_name) > -1:
+                long_game_name[index] = new_game
+                short_game_name.append(longer_name)
+                flag = False
+                break
+        if flag:
+            long_game_name.append(new_game)
+        flag = True
 
-
-for i in range(len(game_name)-1):
-	for j in range(i+1,len(game_name)):
-		if i == j:
-			continue
-		if game_name[i].find(game_name[j])>-1:
-			needxx.append(j)
-		elif game_name[j].find(game_name[i])>-1:
-			needxx.append(i)
-
-x2pop = 0
-for idx in needxx:
-	game_name.pop(idx-x2pop)
-	x2pop +=1
-
-with open("gamename.txt", "w", encoding="utf-8") as f:
-	for item in game_name:
-		f.write(item)
-		f.write("\n")
+long_game_name.pop(0)
