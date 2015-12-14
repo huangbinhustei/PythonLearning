c23d82c76d66ee9226708cd85b761756a52895cb
彬歆 <huangbinhustei@gmail.com>
2015-12-15 00:00:34 +0800

    当乐

--- network/v2bg.py
+++ network/v2bg.py
@@ -3,37 +3,24 @@ import re
 from datetime import datetime
 from pyquery import PyQuery as pq
 
+target_url = request.urlopen("http://ng.d.cn/channel/testlist.html").read()
 
-target_url = request.urlopen("http://ng.d.cn/channel/testlist.html").read().decode("utf-8")
-start = datetime.now()
-pattern1 = r'<td class="name"><a href=.+<h2 class="yjkf">已经开测</h2>'
-pattern2 = r'</tr>'
-target = re.split(pattern2, re.findall(pattern1, target_url, re.S)[0])
+res = pq(target_url)("tr")
+spaces = re.compile(r' {2,11}')
 
-next2result = []
-result = []
-final = []
-for item in target:
-    small_item = re.findall(r'<td.+</td>', item)
-    for item_kuaile in small_item:
-        next2result.append(item_kuaile)
-for i in range(len(next2result)):
-    if i % 6 == 0 or i % 6 ==3:
-        result.append(next2result[i])
-
-a = "".join(result)
-
-v_source = pq(a)
-b = v_source("td")
-for i in pq(a):
-    final.append(pq(i).text().split(" "))
-
-print(final)
-for_print = []
-
-# for i in range(len(final)):
-#     if not for_print[i//4]:
-#         for_print.append("")
-#     for_print[i//4] = "".join(final[i])
-#
-# print(for_print)
+today = datetime.now()
+i = True
+for item in res:
+    remove_space_1 = pq(item).text().replace(u"\xa0\r\n\t", u"")
+    remove_space_2 = re.sub(spaces, "|", remove_space_1)
+    temp_list = remove_space_2.strip().split(" ")
+    if i:
+        i = False
+        continue
+    try:
+        date = temp_list[2].split("-")
+        last_second = datetime(2015, int(date[0]), int(date[1]), 23, 59)
+        if last_second > today:
+            print(temp_list[:3])
+    except:
+        pass
