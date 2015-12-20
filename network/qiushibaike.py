8ae70ba458ad8e788db55fec88e25eb48dfe017a
彬歆 <huangbinhustei@gmail.com>
2015-12-20 23:42:49 +0800

    1

--- network/qiushibaike.py
+++ network/qiushibaike.py
@@ -4,27 +4,21 @@ from pyquery import PyQuery as pq
 
 
 page_number = 1
-url = "http://www.qiushibaike.com/8hr/page/" + str(page_number)
-target = requests.get(url).content.decode("utf-8")
-target = target.replace(u"\xa0", u"")
-flags = pq(target)("div")
-flags = pq(flags).children()
-for item in flags:
-    x = pq(item).text()
-    print(type(x))
-    x = x.encode("cp936")
-    print(x.decode("cp936"))
-    # print(pq(item).text())
+last = []
+while 1:
+    if page_number == 10:
+        break
+    url = "http://www.qiushibaike.com/8hr/page/" + str(page_number)
+    target = requests.get(url).content.decode("utf-8")
+    flag_content = pq(target)(".content")
+    for item in flag_content:
+        a = pq(item).text()
+        if len(a) > 20:
+            last.append(a.replace(" ", "") + "\n")
+    page_number += 1
 
-
-
-
-
-
-
-
-# flags = re.findall(r'<div class="content">(.+?)</div>', target, re.S)
-# for item in flags:
-#     print(item)
-
-
+for item in last:
+    try:
+        print(item)
+    except Exception as e:
+        print(e)
