29a269ef005cd6d94f55fffc659aa8d1bdb85cab
彬歆 <huangbinhustei@gmail.com>
2016-01-01 18:57:20 +0800

    1

--- recommending/sug.py
+++ recommending/sug.py
@@ -1,12 +1,12 @@
 import heapq
 import jieba
 import os
-from datetime import datetime
+# from datetime import datetime
 from collections import defaultdict
+import sqlite3
 
-start = datetime.now()
-user_dict = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)) + "/DATA/userdict/gamename.txt"
-result_file = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)) + "/DATA/weight.txt"
+user_dict = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)) + "/DATA/user_dict/game_name_list.txt"
+path = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)) + "/DATA/doc_list.txt"
 
 
 def weight_init():
@@ -18,54 +18,71 @@ def weight_init():
 
 
 weights = defaultdict(weight_init)
+doc_dict = defaultdict(lambda: "")
+
+
+def load_dict_of_id2title():
+    with open(path, "r", encoding="utf-8") as docs:
+        for line in docs:
+            temp = line.split("\t")
+            doc_dict[str(temp[0])] = temp[1]
 
 
 def init_self():
-    with open(result_file, "r", encoding="utf-8") as w:
-        for line in w:
-            temp = line.split("\t")
-            weights[temp[0]][0] = float(temp[1])
-            weights[temp[0]][1] = temp[2][1:-2].split(",")
-            weights[temp[0]][2] = temp[3].strip().split(",")
-
     with open(user_dict, "r", encoding="utf-8") as ud:
         for line in ud.readlines():
             xxx = line.strip()
             jieba.add_word(xxx)
 
-    mid = datetime.now()
-    print(mid - start)
+
+def segmentation2sql(sentence):
+    key_words = set(jieba.cut(sentence, cut_all=False))
+    temp = []
+    for item in key_words:
+        if item != " ":
+            temp.append("\"" + item + "\"")
+    return "select key,key_weight,docs from keywords where key in (" + ",".join(temp) + ")"
 
 
-def do_sug(target):
+def format_ans(list_target):
     sug_result = []
-    outputs = defaultdict(lambda: 0)
-    key_words = set(jieba.cut(target, cut_all=False))
-    # print(key_words)
-    for items in key_words:
-        weight = weights[items][0]
-        if weight <= 0:
-            continue
-        for title in set(weights[items][1]):
-            outputs[title] += weight
-    for key in outputs:
-        outputs[key] /= len(key) + 1
-    for_print = heapq.nlargest(10, outputs.items(), lambda x: x[1])
+    for_print = heapq.nlargest(10, list_target.items(), lambda x: x[1])
     for item in for_print:
-        print("\t" + str(item[0]) + "\t" + str(item[1]))
-        sug_result.append("\t" + str(item[0]) + "\t" + str(item[1]))
+        true_name = doc_dict[str(item[0])]
+        print("\t" + true_name + "\t" + str(item[1]))
+        sug_result.append("\t" + true_name + "\t" + str(item[1]))
     return sug_result
 
 
-# while True:
-#     init_self()
-#     query = input("Query, please!\n")
-#     if query == "exit":
-#         print("See you!")
-#         break
-#     do_sug(query)
+def do_sug(target):
+    outputs = defaultdict(lambda: 0)
+
+    str_sql = segmentation2sql(target)
+    conn = sqlite3.connect("weight.db")
+    cursor = conn.cursor()
+    try:
+        cursor.execute(str_sql)
+        values_temp = cursor.fetchall()
+        for sql_item in values_temp:
+            if not sql_item:
+                continue
+            weight = values_temp[0][1]
+            for doc_id in str(values_temp[0][2]).split(","):
+                outputs[doc_id] += weight
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.commit()
+        conn.close()
+
+    for key in outputs:
+        outputs[key] /= len(key) + 1
+
+    return format_ans(outputs)
 
 
 def me_main(query_trans_in):
     init_self()
+    load_dict_of_id2title()
     return do_sug(query_trans_in)
