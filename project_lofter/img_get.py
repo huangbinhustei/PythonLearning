222a5ed8e3090e3caef1139041e5c4f0351803b3
huangbinhustei <huangbinhustei@gmail.com>
2016-12-05 00:50:30 +0800

    imgget

--- project_lofter/img_get.py
+++ project_lofter/img_get.py
@@ -6,6 +6,8 @@ import re
 from picsave import PicSave
 import time
 import logging
+import threading
+from functools import reduce
 logging.basicConfig(level=logging.INFO)
 
 img_src_has_try2save = set([])
@@ -25,9 +27,9 @@ target_list = [
         re.compile(r'http://idheihei\.lofter\.com/post/(.+)')
     )
 ]
-
+target_item = target_list[2]
 # http://12ofme.lofter.com/
-target_item = target_list[1]
+
 
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
@@ -38,25 +40,6 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.130 Safari/537.36',
     # 'Referer': 'http://www.163.com/special/00774IMC/lofter_v13.html',
 }
-
-
-def get_img_src(tar_url):
-    # 根据post的地址，提取img的地址
-    temp_set_of_img_src = set([])
-    r = requests.get(tar_url)
-    if r.status_code != 200:
-        logging.error("打不开：" + tar_url + str(r.status_code))
-        return set([])
-    target_content = r.content
-    target_div = pq(target_content)("div")
-    target_img = pq(target_div)(x_path_post[2])
-    target_a = pq(target_img)("a")
-    for item in target_a:
-        img_src_item = pq(item).attr("bigimgsrc")
-        if img_src_item not in img_src_has_try2save:
-            temp_set_of_img_src.add(img_src_item)
-            img_src_has_try2save.add(img_src_item)
-    return temp_set_of_img_src
 
 
 def get_post_url(target_url):
@@ -74,40 +57,72 @@ def get_post_url(target_url):
 
 
 class Student(object):
+    page_offset = 30
     def __init__(self, url_begin):
         self.set_of_img_src = set([])
-        self.ind = 0
         self.url = url_begin
-        self.pattern = pat
-        self.temp = set([])  # 用来判断是否抓取完毕的，感觉略诡异
-        self.flag = True
+        self.flag = []
+        self.big_flag = True
+        self.save_log = []
+
+    def get_img_src_and_save(self, tar_url):
+        post_name = str(re.sub(pat, r'\1', tar_url))
+        if post_name in post_has_try2save:
+            # 整理过了，需要查漏补缺，现在先跳过
+            logging.info("done" + post_name)
+            self.flag.append(False)
+            return
+        post_has_try2save.add(post_name)
+        self.flag.append(True)
+
+        # 根据post的地址，提取img的地址
+        temp_set_of_img_src = set([])
+        r = requests.get(tar_url)
+        if r.status_code != 200:
+            logging.error("打不开：" + tar_url + str(r.status_code))
+            return
+
+        for item in pq(r.content)("div")(x_path_post[2])("a"):
+            img_src_item = pq(item).attr("bigimgsrc")
+            if img_src_item not in img_src_has_try2save:
+                temp_set_of_img_src.add(img_src_item)
+                img_src_has_try2save.add(img_src_item)
+        new_save_p = PicSave(temp_set_of_img_src, post_name, path)
+        new_save_p.start_save()
+
+    def finish_check(self):
+        if self.flag:
+            self.big_flag = reduce(lambda x, y: x and y, self.flag)
+        else:
+            self.big_flag = False
 
     def start(self):
         # 构造列表页（翻页）的地址，并分别提取每一页
-        page_number = 33
-        while self.flag:
-            page_url = self.url + "?page=" + str(page_number)
-            if page_number == 1:
+        page_offset = 0
+        while self.big_flag:
+            page_offset += 1
+            page_url = self.url + "?page=" + str(page_offset)
+            if 1 == page_offset:
                 page_url = self.url
-            page_number += 1
             logging.info(page_url)
-            self.func_in_try(page_url)  # 一页一页的抓取
-            time.sleep(1)
+            self.flag = []
 
-    def func_in_try(self, this_url):
-        for post_url in get_post_url(this_url):
-            post_name = str(re.sub(self.pattern, r'\1', post_url))
-            if post_name in post_has_try2save:
-                # 整理过了，需要查漏补缺，现在先跳过
-                logging.info(post_name)
-                # self.flag = False
-                continue
-            post_has_try2save.add(post_name)
-            img_set = get_img_src(post_url)  # img_list = 这一页的图片url list
-            new_save_p.update(img_set, post_name)
-            new_save_p.start_save()
-            self.flag = True
-        return True
+            post_url_set = get_post_url(page_url)
+
+            th = []
+            for post_url in post_url_set:
+                th.append(threading.Thread(target=self.get_img_src_and_save, args=(post_url,)))
+            for t in th:
+                t.setDaemon(True)
+                t.start()
+            for t in th:
+                t.join()
+
+            self.finish_check()
+            time.sleep(0.5)
+
+        re_save_p = PicSave(set([]), " ", path)
+        re_save_p.re_save_by_log()
 
 
 if __name__ == '__main__':
@@ -124,5 +139,4 @@ if __name__ == '__main__':
         post_has_try2save = set([])
     pat = target_item[2]
     new_lol = Student(url)
-    new_save_p = PicSave({}, " ", path)
     new_lol.start()
--- project_lofter/picsave.py
+++ project_lofter/picsave.py
@@ -1,12 +1,11 @@
 # -*- coding: utf-8 -*-
 import requests
 import os
-from pyquery import PyQuery as pq
 import hashlib
 import threading
 from PIL import Image
 
-min_pic_size = 500000
+min_pic_size = 300000
 
 
 def get_md5(need2md5):
@@ -17,7 +16,6 @@ def get_md5(need2md5):
 
 class PicSave(object):
     def __init__(self, url_set, post_name, basedir, retry_time=3):
-        self.set_of_img_src = set([])
         self.pic_url_set = url_set
         self.save_log = []
         self.post_name = post_name
@@ -33,7 +31,6 @@ class PicSave(object):
             self.basedir = basedir_new
 
     def save_one_picture(self, item_url):
-
         flag = "failed"
         pic_name = str(get_md5(item_url)) + ".jpg"
         pic_path = self.basedir + self.post_name + "_" + pic_name
@@ -41,6 +38,9 @@ class PicSave(object):
             try:
                 pic_target = requests.get(item_url, timeout=20)
             except Exception as e:
+                continue
+            if 404 == pic_target.status_code:
+                flag = "404"
                 continue
             c_length = pic_target.headers["Content-Length"]
             if not c_length or int(c_length) < min_pic_size:
@@ -51,9 +51,14 @@ class PicSave(object):
             saved_size = os.path.getsize(pic_path)
             if int(c_length) == int(saved_size):
                 flag = "done = " + str(i+1) + " " + str(c_length)
-                self.set_of_img_src.add(item_url)
                 break
         self.save_log.append([self.post_name, item_url, pic_name, flag])
+        return [self.post_name, item_url, pic_name, flag]
+
+    def log_writing(self, kind):
+        with open(self.basedir + "log.txt", kind) as s_log:
+            for list_item in self.save_log:
+                s_log.write("\t".join(list_item) + "\n")
 
     def start_save(self):
         if not self.pic_url_set:
@@ -67,6 +72,21 @@ class PicSave(object):
             t.start()
         for t in th:
             t.join()
-        with open(self.basedir + "log.txt", "a") as s_log:
-            for list_item in self.save_log:
-                s_log.write("\t".join(list_item) + "\n")
+        self.log_writing("a")
+
+    def re_save_by_log(self):
+        print("\nre_save_by_log\n")
+        re_write_flag = False
+        with open(self.basedir + "log.txt", "r") as f:
+            lines = [line.strip().split("\t") for line in f.readlines()]
+
+        self.save_log = []
+        for line in lines:
+            if "done" in line[3] or "small" in line[3] or "404" in line[3]:
+                self.save_log.append(line)
+            else:
+                re_write_flag = True
+                self.post_name = line[0]
+                self.save_one_picture(line[1])
+        if re_write_flag:
+            self.log_writing("w")
