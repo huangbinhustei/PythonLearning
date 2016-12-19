481f91c3b6f23ba05f1b43bc905168a5eb99b97c
彬歆 <huangbinhustei@gmail.com>
2016-12-19 22:57:03 +0800

    1

--- gonglve/mycomposition/views.py
+++ gonglve/mycomposition/views.py
@@ -2,34 +2,46 @@
 # -*- coding: utf-8 -*-
 
 from data import Docs, db, app
-from flask import Flask, request, sessions, g, redirect, render_template, url_for, jsonify
+from flask import request, sessions, g, redirect, render_template, url_for, jsonify
 from sqlalchemy import desc, and_
 import time
 import logging
 from collections import defaultdict
 import re
+from datetime import datetime
 
 logging.basicConfig(level=logging.INFO)
-par = re.compile("\.[a-z0-9A-Z]+{display: none;} 精彩内容，尽在百度攻略：http://gl.baidu.com")
+par = re.compile("\.[a-z0-9A-Z]+\{display: none;} 精彩内容，尽在百度攻略：http://gl\.baidu\.com")
+
+
+def view_counts(t_doc):
+    t_doc.view += 1
+    today = int(time.time()/86400)
+    if t_doc.update_time == today:
+        t_doc.today_view += 1
+    elif t_doc.update_time + 1 == today:
+        t_doc.update_time = today
+        t_doc.today_view = 1
+        t_doc.yesterday_view = t_doc.today_view
+    else:
+        t_doc.update_time = today
+        t_doc.today_view = 1
+    db.session.commit()
 
 
 @app.route("/view/<page_md>", methods=['GET'])
 def page_view(page_md):
-    a = time.time()
     doc = Docs.query.filter(Docs.doc_md == page_md).first()
-    print(time.time() - a)
     if not doc:
         return jsonify({"error": "no such md"})
 
     entry = doc.to_dict()
-    
     entry["content"] = []
     for item in re.sub(par, "\t", doc.content).split("\t"):
         for s in item.split(" "):
             entry["content"].append(s)
-    doc.view += 1
-    db.session.commit()
-    return render_template("view.html",entry=entry)
+    view_counts(doc)
+    return render_template("view.html", entry=entry)
     
 
 @app.route("/api/view/<page_md>", methods=['GET'])
@@ -41,17 +53,14 @@ def api_view(page_md):
         return jsonify({"error": "no such md"})
     target = request.args.get('target')
     if target:
-        return jsonify({target:doc.to_dict()[target]})
+        return jsonify({target: doc.to_dict()[target]})
     return jsonify(**doc.to_dict())
 
 
 @app.route("/api/list", methods=['GET'])
 def api_list():
     a = time.time()
-    grade = request.args.get('grade')
-    genre = request.args.get('genre')
-    words = request.args.get('words')
-
+    grade, genre, words = (request.args.get('grade'), request.args.get('genre'), request.args.get('words'))
     query = Docs.query
     if grade:
         query = query.filter(Docs.grade == grade)
@@ -64,6 +73,12 @@ def api_list():
         lists[item.doc_md] = item.to_dict()
     print(time.time() - a)
     return jsonify(**lists)
+
+
+@app.before_request
+def before_request():
+    this_url = str(request.url).replace("http://127.0.0.1:5000/", "")
+    logging.info(str(datetime.now()) + "\t" + this_url)
 
 
 if __name__ == '__main__':
