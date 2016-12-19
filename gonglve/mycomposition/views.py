d1bf1ae2e33ceb127601ebd28a4c70f1bf8d1e13
huangbinhustei <huangbinhustei@gmail.com>
2016-12-19 19:44:49 +0800

    页面

--- gonglve/mycomposition/views.py
+++ gonglve/mycomposition/views.py
@@ -7,22 +7,47 @@ from sqlalchemy import desc, and_
 import time
 import logging
 from collections import defaultdict
+import re
 
 logging.basicConfig(level=logging.INFO)
+par = re.compile("\.[a-z0-9A-Z]+{display: none;} 精彩内容，尽在百度攻略：http://gl.baidu.com")
 
 
-@app.route("/api/view/<page_md>", methods=['GET'])
-def api_view(page_md, target=""):
+@app.route("/view/<page_md>", methods=['GET'])
+def page_view(page_md):
     a = time.time()
     doc = Docs.query.filter(Docs.doc_md == page_md).first()
     print(time.time() - a)
     if not doc:
         return jsonify({"error": "no such md"})
+
+    entry = doc.to_dict()
+    
+    entry["content"] = []
+    for item in re.sub(par, "\t", doc.content).split("\t"):
+        for s in item.split(" "):
+            entry["content"].append(s)
+    doc.view += 1
+    db.session.commit()
+    return render_template("view.html",entry=entry)
+    
+
+@app.route("/api/view/<page_md>", methods=['GET'])
+def api_view(page_md):
+    a = time.time()
+    doc = Docs.query.filter(Docs.doc_md == page_md).first()
+    print(time.time() - a)
+    if not doc:
+        return jsonify({"error": "no such md"})
+    target = request.args.get('target')
+    if target:
+        return jsonify({target:doc.to_dict()[target]})
     return jsonify(**doc.to_dict())
 
 
 @app.route("/api/list", methods=['GET'])
 def api_list():
+    a = time.time()
     grade = request.args.get('grade')
     genre = request.args.get('genre')
     words = request.args.get('words')
@@ -34,10 +59,10 @@ def api_list():
         query = query.filter(Docs.genre == genre)
     if words:
         query = query.filter(Docs.words >= words)
-
     lists = defaultdict(lambda: 0)
     for item in query.paginate(0, 20, False).items:
         lists[item.doc_md] = item.to_dict()
+    print(time.time() - a)
     return jsonify(**lists)
 
 
