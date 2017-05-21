95f59d25ee791adcda723e1597b6f2f5bedcf0c3
彬歆 <huangbinhustei@gmail.com>
2017-05-21 18:11:25 +0800

    解决了部分防守时犯蠢的问题

--- Gomokuy/loc_gui.py
+++ Gomokuy/loc_gui.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Flask, render_template, request, redirect
-from gomoku import Calc
+from flask import Flask, render_template, request, redirect, url_for
+from gomoku import Calc, Game, Danger, B, W
 
 app = Flask(__name__)
 
@@ -11,10 +11,23 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     loc = request.args.get('loc')
-    if game.over:
+    retract = request.args.get('retract')
+    restart = request.args.get('restart')
+    if restart:
+        # 重启游戏
         game.restart()
-        return render_template("home.html", game=game)
+        fir = request.args.get('aifirst')
+        if fir == "1":
+            game.going((5, 5))
+        return redirect(url_for("home"))
+
+    if retract and retract == "1":
+        # 悔棋
+        game.retract()
+        return redirect(url_for("home"))
+
     if not loc:
+        # 第一次打开，直接开始新游戏
         return render_template("home.html", game=game)
     # 玩家下一步棋开始
     loc = int(loc)
@@ -23,11 +36,20 @@ def home():
     # 玩家下一步棋结束
 
     # 电脑下一步棋开始
-    game.calculator()
+    player = B if (game.step + 1) % 2 == 1 else W
+    d = Danger(game.grid, player)
+    pos = d.calc()
+
+    if pos:
+        game.going(pos)
+    else:
+        pos = game.calculator()
+        game.going(pos)
     # 电脑下一步棋结束
     return redirect(request.referrer)
 
 
 if __name__ == '__main__':
     game = Calc()
-    app.run(debug=True)
+    # game = Game()
+    app.run(host="0.0.0.0", debug=True)
