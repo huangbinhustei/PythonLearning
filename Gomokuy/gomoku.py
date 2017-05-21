e5e964b98b73d8c1faad1efcb8f0b9cdb9e6cf1b
彬歆 <huangbinhustei@gmail.com>
2017-05-21 18:11:25 +0800

    解决了部分防守时犯蠢的问题

--- Gomokuy/gomoku.py
+++ Gomokuy/gomoku.py
@@ -2,6 +2,8 @@
 # -*- coding: utf-8 -*-
 
 from collections import defaultdict
+from random import choice
+from functools import reduce
 
 B = 1
 W = 2
@@ -11,6 +13,120 @@ ROADS = {
     2: (1, 1),
     3: (1, -1),
 }
+SCORE = {
+    # True ：自己，False：对方
+    True: {
+        "活4": 100000,
+        "冲4": 100000,
+        "活3": 1000,
+        "冲3": 1000,
+        "活2": 1,
+    },
+    False: {
+        "活4": 10000,
+        "冲4": 10000,
+        "活3": 10,
+    }
+}
+
+
+class Danger:
+    def __init__(self, grid, color):
+        self.grid = grid
+        self.res = dict()
+        self.width = len(grid)
+        self.color = color
+        self.opponent = B if color == W else W
+
+    def __value_calc_single_chessman(self, row, col, color, opponent):
+        ret = []
+        for direction in range(4):
+            liner = [[], [(row, col)], []]
+            for side in (-1, 1):
+                side_length = len(liner[1 + side]) + len(liner[1])
+                if side_length >= 5:
+                    continue
+                for offset in range(1, 5):
+                    new_row = row + offset * side * ROADS[direction][0]
+                    new_col = col + offset * side * ROADS[direction][1]
+                    if new_row >= self.width or new_row < 0:
+                        break
+                    if new_col >= self.width or new_col < 0:
+                        break
+
+                    new_cell = self.grid[new_row][new_col]
+                    new_loc = (new_row, new_col)
+                    if new_cell == color:
+                        liner[1].append(new_loc)
+                    elif new_cell == 0:
+                        liner[1 + side].append(new_loc)
+                    elif new_cell == opponent:
+                        break
+            liner[1] = sorted(sorted(liner[1], key=lambda x: x[1]), key=lambda x: x[0])  # 处理liner
+            liner[0] = liner[0][:5 - len(liner[1])]
+            liner[2] = liner[2][:5 - len(liner[1])]
+
+            if len(liner[0]) + len(liner[1]) + len(liner[2]) >= 5:
+                ret.append(liner)
+        return ret
+
+    def __calc_single_color(self, color):
+        def get_danger_area(l, length, kid):
+            if length < 2:
+                return []
+            if "活" in kid:
+                ret = l[0][:1]
+                ret += l[2][:1]
+            else:
+                ret = l[0][:5 - length]
+                ret += l[2][:5 - length]
+            return ret
+
+        all_lines = []
+        self.res[color] = defaultdict(list)
+        opponent = B if color == W else W
+        for row in range(self.width):
+            for col in range(self.width):
+                if self.grid[row][col] == color:
+                    temp = self.__value_calc_single_chessman(row, col, color, opponent)
+                    for line in temp:
+                        if line not in all_lines:
+                            all_lines.append(line)
+        for line in all_lines:
+            chang = len(line[1])
+            if line[0] and line[2]:
+                key = "活" + str(chang)
+            elif line[0] or line[1]:
+                key = "冲" + str(chang)
+            else:
+                continue
+            area = get_danger_area(line, chang, key)
+            if area:
+                self.res[color][key].append(area)
+
+    def calc(self):
+        for color in (B, W):
+            self.__calc_single_color(color)
+        return self.choosing()
+
+    def choosing(self):
+        status = defaultdict(int)
+        for side, sd in self.res.items():
+            for kid, loc_groups in sd.items():
+                for loc_group in loc_groups:
+                    for loc in loc_group:
+                        if kid in SCORE[side == self.color]:
+                            status[loc[0] * self.width + loc[1]] += SCORE[side == self.color][kid]
+
+        status = sorted(status.items(), key=lambda x: x[1], reverse=True)
+        status = [(int(item[0]/self.width), int(item[0] % self.width), item[1]) for item in status]
+        if status:
+            return status[0][:2]
+        else:
+            return False
+
+    def __repr__(self):
+        return self.res
 
 
 class Game:
@@ -86,7 +202,7 @@ class Game:
         player = B if self.step % 2 == 1 else W
         print(f"{player}: {loc}")
         self.grid[loc_x][loc_y] = player
-        self.records.append(loc)
+        self.records.append(list(loc))
         self._ending(loc, player)
 
     def retract(self):
@@ -150,7 +266,7 @@ class Calc(Game):
                         liner[1 + side].append(new_loc)
                     elif new_cell == opponent:
                         break
-            liner[1] = sorted(sorted(liner[1], key=lambda x: x[1]), key=lambda x: x[0])     # 处理liner
+            liner[1] = sorted(sorted(liner[1], key=lambda x: x[1]), key=lambda x: x[0])  # 处理liner
             ret.append(liner)
         return ret
 
@@ -189,7 +305,7 @@ class Calc(Game):
 
         temp = sorted(self.val.items(), key=lambda x: x[1], reverse=True)[0]
         temp = [int(temp[0] / self.width), int(temp[0] % self.width)]
-        self.going(temp)
+        return temp
 
 
 def side_func_pvp():
@@ -216,4 +332,19 @@ def side_func_pve():
 
 
 if __name__ == '__main__':
-    side_func_pve()
+    a = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 2, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+    d = Danger(a, B)
+    d.calc()
+    print(d.res)
--- Gomokuy/loc_gui.py
+++ Gomokuy/loc_gui.py
@@ -2,18 +2,38 @@
 # -*- coding: utf-8 -*-
 
 
-from flask import Flask, render_template, request, redirect
-from gomoku import Calc
+from flask import Flask, render_template, request, redirect, url_for
+from gomoku import Calc, Game, Danger
 
 app = Flask(__name__)
+
+B = 1
+W = 2
+a = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 2, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 2, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 
 @app.route("/")
 def home():
     loc = request.args.get('loc')
-    if game.over:
+    restart = request.args.get('restart')
+    if restart:
         game.restart()
-        return render_template("home.html", game=game)
+        fir = request.args.get('aifirst')
+        if fir == "1":
+            game.going((5, 5))
+        return redirect(url_for("home"))
     if not loc:
         return render_template("home.html", game=game)
     # 玩家下一步棋开始
@@ -23,11 +43,20 @@ def home():
     # 玩家下一步棋结束
 
     # 电脑下一步棋开始
-    game.calculator()
+    player = B if (game.step + 1) % 2 == 1 else W
+    d = Danger(game.grid, player)
+    pos = d.calc()
+
+    if pos:
+        game.going(pos)
+    else:
+        pos = game.calculator()
+        game.going(pos)
     # 电脑下一步棋结束
     return redirect(request.referrer)
 
 
 if __name__ == '__main__':
     game = Calc()
-    app.run(debug=True)
+    # game = Game()
+    app.run(host="0.0.0.0", debug=True)
