b6946aec4b4ed2031097287bc475c2ebabddec1b
彬歆 <huangbinhustei@gmail.com>
2018-03-18 23:26:07 +0800

    增加互信息和左右信息熵的筛选
    互信息没有实现。

--- segmentation/seg.py
+++ segmentation/seg.py
@@ -3,6 +3,7 @@ import os
 import time
 import logging
 import math
+import sys
 from collections import defaultdict
 from multiprocessing import Pool
 
@@ -11,112 +12,257 @@ logger.setLevel(logging.DEBUG)
 base = os.path.abspath(os.path.dirname(__file__))
 
 
+class ProgressBar:
+    def __init__(self, count=0, total=0, width=50):
+        self.count = count
+        self.total = total
+        self.width = width
+
+    def move(self):
+        self.count += 1
+        sys.stdout.write(' ' * (self.width + 9) + '\r')
+        sys.stdout.flush()
+        progress = int(self.width * self.count / self.total)
+        sys.stdout.write('{0:3}/{1:3}: '.format(self.count, self.total))
+        sys.stdout.write('+' * progress + '-' * (self.width - progress) + '\r')
+        if progress == self.width:
+            sys.stdout.write('\n')
+        sys.stdout.flush()
+
+
 class SEG:
     def __init__(self, cont, title="default", max_length=10, stops=""):
-        self.title = "《" + title + "》"
         self.start = time.time()
+
+        self.title = title
         self.cont = cont
+        self.line = ""  # 将文章合并为单行
+        self.size = 0
+        # 以上四个是文章的属性
+
         self.stops = stops
         self.max_length = max_length
-        self.door = int(len(",".join(self.cont)) / 100000) + 2
+        self.door = int(self.size / 10000) + 3
+
+        self.word = {
+            "candidates": defaultdict(int),
+            "book": defaultdict(int),
+        }
+
         self.en = defaultdict(int)
-        self.zh = defaultdict(int)
-        self.book = defaultdict(int)
-        self.result = dict()
+        self.phrase = defaultdict(int)
+
         self.not_word = set([])
+        self.neighbor = dict()
 
-        print(f"{self.title}加载完成，阈值 {self.door}，开始分析@{time.ctime()}")
+        self.__screening()
 
-    def booker(self):
-        book_mark = re.compile("《.+?》")
-        for ind, line in enumerate(self.cont):
-            for item in re.findall(book_mark, line):
-                self.book[item] += 1
-            self.cont[ind] = re.sub(book_mark, "\t", line)
+    def __screening(self):
+        """
+        对文本进行预处理，包括：
+        1. 提取所有成对书名号中的内容，存入 self.word["book"]
+        2. 将所有标点符号，替换为 $
+        3. 处理后的文章合并为一行
+        4. 计算文章总长度
+        5. 将文章组成词对
+        """
 
-    def filter(self):
-        zp = re.compile(u'[\u4e00-\u9fa5]+')
-        par = re.compile("[()（）:，.、。 ？！；：“”…‘’]")
-        tmp = []
+        def booker():
+            book_mark = re.compile("《.+?》")
+            for line in self.cont:
+                for item in re.findall(book_mark, line):
+                    self.word["book"][item] += 1
 
-        cont = [re.sub(par, "\t", line) for line in self.cont]
-        self.cont = sum([i.split("\t") for i in cont], [])
+        def englisher():
+            zp = re.compile(u'[\u4e00-\u9fa5]+')
+            tmp = []
+            for line in self.cont:
+                if not zp.search(line):
+                    self.en[line] += 1
+                else:
+                    tmp.append(line)
+            self.cont = tmp
 
-        for item in self.cont:
-            if not item:
+        def punctuationer():
+            self.cont = [re.sub(punctuation, "$", line) for line in self.cont if line]  # 除书名号外，其他符号被处理成$
+            self.line = "$" + "$".join(self.cont) + "$"
+            self.size = len(self.line)
+
+        def phraser():
+            def grouping(extent):
+                for line in self.cont:
+                    if len(line) <= extent:
+                        # 为什么是 continue？
+                        continue
+                    for cell in [line[i:i + extent + 1] for i in range(len(line) - extent)]:
+                        self.phrase[cell] += 1
+                        if re.findall(punctuation, cell):
+                            self.not_word.add(cell)
+
+            for i in range(self.max_length):
+                grouping(i)
+
+        punctuation = re.compile("[（）、.，。 ？！：；$“”…‘’ \t\n]")
+        booker()
+        englisher()
+        punctuationer()
+        phraser()
+
+    def _show_top_tf(self):
+
+        print("+-%-24s-+-%-6s-+-%-6s-+" % ("-" * 24, "-" * 6, "-" * 6))
+        space_title = ' ' * (25 - 2 * len(self.title) - 11)
+        print(f"| Keywords @ {self.title}{space_title}| {' '*4}TF | {' '*3}PMI |")
+        print("+-%-24s-+-%-6s-+-%-6s-+" % ("-" * 24, "-" * 6, "-" * 6))
+        for w, freq in sorted(self.word["candidates"].items(), key=lambda x: x[1], reverse=True)[:10]:
+            space_fi = ' ' * (25 - 2 * len(w))
+            space_sec = ' ' * (6 - len(str(freq)))
+            w_pmi = round(self.filtering_pmi(w, freq), 3)
+            space_thi = ' ' * (6 - len(str(w_pmi)))
+            print(f"| {w}{space_fi}| {space_sec}{freq} | {space_thi}{w_pmi} |{self.neighbor[w]}")
+        print("+-%-24s-+-%-6s-+-%-6s-+" % ("-" * 24, "-" * 6, "-" * 6))
+
+    def _neighbourhood_for_single_word(self, word):
+        # todo :word 太长的时候，下面的算法不对
+
+        left = defaultdict(int)
+        right = defaultdict(int)
+
+        par = re.compile('.' + word + '.')
+        for item in re.findall(par, self.line):
+            left[item[0]] += 1
+            right[item[-1]] += 1
+
+        freq_left = [v for k, v in left.items()]
+        total_left = sum(freq_left)
+        freq_left = [v / total_left for v in freq_left]
+        freq_right = [v for k, v in right.items()]
+        total_right = sum(freq_right)
+        freq_right = [v / total_right for v in freq_right]
+
+        ret = [
+            int(sum([-i * math.log(i) for i in freq_left]) * 100),
+            int(sum([-i * math.log(i) for i in freq_right]) * 100)
+        ]
+        if max(ret) <= 100:
+            self.not_word.add(word)
+
+        return ret
+
+    def filtering_neighbourhood(self):
+        for word, freq in self.phrase.items():
+            if word in self.not_word:
                 continue
-            elif not zp.search(item):
-                # 不包含中文
-                self.en[item] += 1
-            else:
-                tmp.append(item)
-        self.cont = tmp
-
-    def grouping(self, lth):
-        tmp = defaultdict(int)
-        for line in self.cont:
-            if len(line) <= lth:
+            elif freq < self.door:
                 continue
-            for cell in [line[i:i + lth + 1] for i in range(len(line) - lth)]:
-                tmp[cell] += 1
-        for k, v in tmp.items():
-            if v >= self.door:
-                self.zh[k] += v
+            self.neighbor[word] = self._neighbourhood_for_single_word(word)
+
+    def filtering_bayesian(self, word, prob_ab):
+        word_length = len(word)
+
+        sub_words = [word[i: j] for i in range(word_length) for j in range(word_length + 1) if j > i]
+        sub_words.remove(word)
+
+        for sub_word in sub_words:
+            prob_b = self.phrase[sub_word]
+            if len(sub_word) == 1:
+                self.not_word.add(sub_word)
+                # 一个字的都不算词。
+                continue
+            if prob_ab / prob_b > 0.5:
+                # 相当于 P(A|B) = P(AB)/P(B) > 50%，只要 B 出现那么 AB 肯定出现 => B 肯定不是词
+                self.not_word.add(sub_word)
+
+    def filtering_unnamed(self, word, prob_ab):
+        dangers = []
+        for i in range(len(word) - 1):
+            dangers.append(min(self.phrase[word[:i + 1]], self.phrase[word[i + 1:]]))
+        prob_danger = max(dangers)
+        if prob_ab / prob_danger <= 0.5:
+            # 假如父比某个子对的出现概率小太多，那么父词可能不是词
+            self.not_word.add(word)
+
+    def filtering_pmi(self, word, prob_ab):
+        prob_danger = 1
+        for i in range(len(word) - 1):
+            prob_danger = max(self.phrase[word[:i + 1]] * self.phrase[word[i + 1:]], prob_danger)
+        ret = prob_danger / prob_ab / self.size
+        if ret > 0.02:
+            self.not_word.add(word)
+        return ret
 
     def run(self):
-        self.booker()
-        self.filter()
-
-        for i in range(self.max_length):
-            self.grouping(i)
-
-        jmp = set([])
-        for word in sorted(self.zh.keys(), key=lambda x: len(x), reverse=True):
-            if word in jmp or len(word) <= 2:
+        for word in sorted(self.phrase.keys(), key=lambda x: len(x), reverse=True):
+            if word in self.not_word or len(word) == 1 or self.phrase[word] < self.door:
                 continue
+            prob_ab = self.phrase[word]
 
-            word_length = len(word)
-            p_word = self.zh[word]
+            self.filtering_bayesian(word, prob_ab)
+            if len(word) == 2 and word[0] == word[1]:
+                # todo: 叠字在后面两个的处理中非常吃亏，临时过滤掉
+                continue
+            self.filtering_unnamed(word, prob_ab)
+            self.filtering_pmi(word, prob_ab)
+            # todo: PMI 没有提供任何有价值的筛选
 
-            sub_words = [word[i: j] for i in range(word_length) for j in range(word_length + 1) if j > i]
-            sub_words.remove(word)
+        self.filtering_neighbourhood()
 
-            for sub_word in sub_words:
-                if p_word / self.zh[sub_word] > 0.5:
-                    # p(n字对|子对）< 0.5 => 子对不是词
-                    jmp.add(sub_word)
+        for word, freq in self.phrase.items():
+            if word in self.not_word:
+                continue
+            elif len(word) == 1:
+                continue
+            elif word in self.stops:
+                continue
+            self.word["candidates"][word] = freq
 
-            dangers = []
-            for i in range(len(word) - 1):
-                dangers.append(min(self.zh[word[:i + 1]], self.zh[word[i + 1:]]))
-            p_danger = max(dangers)
-            if p_word / p_danger <= 0.5:
-                # 假如父比某个子对的出现概率小太多，那么父词可能不是词
-                jmp.add(word)
+        # self._show_top_tf()
 
-        self.not_word = set([i for i in jmp if len(i) >= 2])
 
-        for k, v in self.zh.items():
-            if k not in jmp and len(k) != 1 and k not in self.stops:
-                self.result[k] = v
-        
+class Book(ProgressBar):
+    def __init__(self, name, paths):
+        self.name = name
+        self.paths = paths
+        self.stops = ""
 
-        # for w, rate in sorted(self.result.items(), key=lambda x: x[1], reverse=True)[:10]:
-            # print(f"{self.title}：\t{self.zh[w]}\t{int(rate)}\t{int(self.zh[w]//rate)}\t{w}")
-        print(f"{self.title}分析完成@{time.ctime()}，耗时{int((time.time()-self.start)*1000)}ms")
+        ProgressBar.__init__(self, total=len(paths))
 
-        
+        self.result = []
+        self.idf = defaultdict(int)
+
+    def _record(self):
+        with open(os.path.join(base, "doc", self.name + "IDF.txt"), "w") as f:
+            for k, v in self.idf.items():
+                f.write(f"{v}\t{k}\n")
+
+    def _show(self):
+        def rank_key(f):
+            return f[1] * math.log(120 / self.idf[f[0]], 120)
+
+        for chapter, title in self.result:
+            for w, freq in sorted(chapter["candidates"].items(), key=rank_key, reverse=True)[:10]:
+                print(f"{freq}\t{self.idf[w]}\t{w}\t{title}\t")
+
+    def run(self):
+        for file, title in self.paths:
+            chapter = SEG(get_content(file), title, stops="")
+            chapter.run()
+
+            self.result.append([chapter.word, title])
+
+            for key, freq in chapter.word["candidates"].items():
+                self.idf[key] += 1
+
+            self.move()
+
+        self._record()
+        self._show()
+
+
 def get_content(path):
-    ret = []
     with open(path, "r", encoding="utf-8") as f:
         ret = [line.strip() for line in f.readlines()]
     return ret
-
-
-def cutting(content, title, _stops):
-    s = SEG(cont=content, title=title, stops=_stops)
-    s.run()
-    return s.result, s.not_word
 
 
 def load_stop_words():
@@ -124,84 +270,16 @@ def load_stop_words():
         return [item.strip() for item in f.readlines()]
 
 
-def single_hander(paths,  _stops):
-    ret = []
-    idf = defaultdict(int)
-    tf = defaultdict(int)
-    neg = defaultdict(int)
-    for file, title in paths:
-        article = get_content(file)
-        tf, _neg = cutting(article, title, _stops)
-
-        for key, freq in tf.items():
-            idf[key] += 1
-            tf[key] += freq
-        for key in _neg:
-            neg[key] += 1
-
-        ret.append([title, tf])
-
-    final_idf = dict()
-    for k, v in idf.items():
-        if k not in neg or v >= neg[k]:
-            final_idf[k] = v
-
-    finnal_neg = dict()
-    for k, v in neg.items():
-        if k not in idf or v >= idf[k]:
-            finnal_neg[k] = v 
-
-    with open(os.path.join(base, "doc", "红楼梦IDF.txt"), "w") as f:
-        for k, v in final_idf.items():
-            # _tmp = []
-            # for sk in final_idf.keys():
-            #     if sk in k and sk != k:
-            #         _tmp.append(sk)
-            #     _p = "\t".join(_tmp)
-            # f.write(f"{v}\t{k}\t{_p}\n")
-            f.write(f"{v}\t{k}\n")
-
-
-    with open(os.path.join(base, "doc", "红楼梦NEG.txt"), "w") as f:
-        for k, v in finnal_neg.items():
-            f.write(f"{v}\t{k}\n")
-
-    def xu(arg):
-        word, freq = arg
-        if word not in neg or freq >= neg[word]:
-            if idf[word] == 1:
-                return freq * math.log(120/idf[word]) / 2
-            return freq * math.log(120/idf[word])
-        return 0
-
-    for title, d in ret:
-        for w, rate in sorted(d.items(), key=xu, reverse=True)[:10]:
-            print(f"{title}\t{int(d[w])}\t{idf[w]}\t{w}")
-
-
-def mult_hander(mult, _stops):
-    p = Pool(min(3, len(mult)))
-    for book, title in mult:
-        p.apply_async(single_hander, args=([book, title], ""))
-    p.close()
-    p.join()
-
-
 def red_dream():
     folder = os.path.join(base, "doc", "红楼梦")
     _, _, files = next(os.walk(folder))
     tasks = [[os.path.join(folder, i), i.replace(".txt", "")] for i in files if i[-3:] == "txt"]
-    single_hander(tasks, "")
+
+    red = Book("红楼梦", tasks)
+    red.run()
 
 
 if __name__ == '__main__':
-    symbols = "-_——+=*/，。？！,.!?\n\t 、:：《》<>\"\';；“”‘’……^"
-    chi_symbols = ":，.、。《》？！；：“”……‘’"
-
-    # ns = [["红楼梦"], ["三国演义"], ["水浒传"]]
-    ns = [[os.path.join(base, "doc", i + ".txt"), i] for i in ["红楼梦", "三国演义", "水浒传"]]
-    
     t = time.time()
     red_dream()
-    # mult_hander(ns, load_stop_words())
-    print(time.time()-t)
+    print("任务完成：" + str(int((time.time() - t) * 1000)) + " ms")
